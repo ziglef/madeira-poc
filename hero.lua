ecce298a6082415d96241ccd9576598fc3f1c54e
Claudio Loseto <loseto@pm.me>
2022-05-02 12:39:16 +0100

    Hero refactored

--- hero.lua
+++ hero.lua
@@ -1,28 +1,59 @@
-local still = true
-local movSpeed = 0.5
 
-function love.load()
-    animation = newAnimation(love.graphics.newImage("hero/down.png"), 16, 16, movSpeed)
+local newAnimation
+
+Hero = {
+    still = true,
+    animation = nil,
+    duration = 1
+}
+
+function Hero:load()
+    -- Load tiles for each direction
+    upAnimation = newAnimation(love.graphics.newImage("assets/hero/up.png"), 16, 16, self.duration)
+    downAnimation = newAnimation(love.graphics.newImage("assets/hero/down.png"), 16, 16, self.duration)
+    rightAnimation = newAnimation(love.graphics.newImage("assets/hero/right.png"), 16, 16, self.duration)
+    leftAnimation = newAnimation(love.graphics.newImage("assets/hero/left.png"), 16, 16, self.duration)
+
+    -- Init hero with down animation
+    self.animation = downAnimation
 end
 
-function love.update(dt)
-    animation.currentTime = animation.currentTime + dt
-    if animation.currentTime >= animation.duration then
-        animation.currentTime = animation.currentTime - animation.duration
-    end
-end
+function Hero:update(dt, duration)
+    self.duration = duration or 1
 
-function love.draw()
-    if still then
-        spriteNum = 1
+    -- Change animation on keypress
+    if love.keyboard.isDown("up") then
+        self.still = false
+        self.animation = upAnimation
+    elseif love.keyboard.isDown("down") then
+        self.still = false
+        self.animation = downAnimation
+    elseif love.keyboard.isDown("left") then
+        self.still = false
+        self.animation = leftAnimation
+    elseif love.keyboard.isDown("right") then
+        self.still = false
+        self.animation = rightAnimation
     else
-        spriteNum = math.floor(animation.currentTime / animation.duration * #animation.quads) + 1
+        self.still = true
     end
-    
-    love.graphics.draw(animation.spriteSheet, animation.quads[spriteNum], 0, 0, 0, 4)
+
+    self.animation.currentTime = self.animation.currentTime + dt
+    if self.animation.currentTime >= self.animation.duration then
+        self.animation.currentTime = self.animation.currentTime - self.animation.duration
+    end
 end
 
-function newAnimation(image, width, height, duration)
+function Hero:draw()
+    local spriteNum = 1
+    if not self.still then
+        spriteNum = math.floor(self.animation.currentTime / self.animation.duration * #self.animation.quads) + 1
+    end
+
+    love.graphics.draw(self.animation.spriteSheet, self.animation.quads[spriteNum], 0, 0, 0, 4)
+end
+
+newAnimation = function(image, width, height, duration)
     local animation = {}
     animation.spriteSheet = image;
     animation.quads = {};
@@ -38,28 +69,3 @@ function newAnimation(image, width, height, duration)
 
     return animation
 end
-
-function love.keypressed(key)
-    still = false
-    if key == 'up' then
-        animation = newAnimation(love.graphics.newImage("hero/up.png"), 16, 16, movSpeed)
-    elseif key == 'down'  then
-        animation = newAnimation(love.graphics.newImage("hero/down.png"), 16, 16, movSpeed)
-    elseif key == 'right'  then
-        animation = newAnimation(love.graphics.newImage("hero/right.png"), 16, 16, movSpeed)
-    elseif key == 'left'  then
-        animation = newAnimation(love.graphics.newImage("hero/left.png"), 16, 16, movSpeed)
-    end
-end
-
-function love.keyreleased(key)
-    if key == 'up' then
-        still = true
-    elseif key == 'down'  then
-        still = true
-    elseif key == 'right'  then
-        still = true
-    elseif key == 'left'  then
-        still = true
-    end
- end
